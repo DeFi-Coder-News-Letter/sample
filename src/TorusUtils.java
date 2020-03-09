0ed71500a8387d8f5eee123f8755ead954ef7317
Leonard Tan <tetratorus@gmail.com>
2020-03-09 19:57:14 +0800

    New project initial commit

---
+++ src/TorusUtils.java
@@ -0,0 +1,12 @@
+public class TorusUtils {
+    boolean logging;
+    public TorusUtils() {
+        logging = false;
+    }
+    public TorusUtils(boolean logging_) {
+        logging = logging_;
+    }
+    public String hello() {
+        return "Hello world";
+    }
+}
